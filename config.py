6da068ef89c91f3481f4ef37a5be875d517f8731
yitinghan03 <119933946+yitinghan03@users.noreply.github.com>
2024-09-01 01:18:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.czof8susvpfx.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "hanyiting-employee1"
+custombucket = "hanyiting-employee"
 customregion = "us-east-1"
 
